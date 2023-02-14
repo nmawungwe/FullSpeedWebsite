a2fb04818d00cc7b3a59d1156ab1dc261fefaf72
Nyasha <nmawungwe@gmail.com>
2023-02-14 15:26:32 +0200

    Added site background styling and font

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -11,26 +11,35 @@ export default function Home() {
         <meta name="description" content="FullSpeed Inc" />
         <Link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>FullSpeed</h1>
-        <p>Check out our latest updates:</p>
-        <ul>
-          <li>
-          <Link href="https://twitter.com/nyashaspeed69">
-            Twitter
-          </Link>
-          </li>
-          <li>
-          <Link href="https://www.instagram.com/nyashaspeed69/">
-           Instagram
-          </Link>
-          </li>
-          {/* // link for substack */}
-          <li>
-          <Link href="https://substack.com/profile/12161427-nyashaspeed69">
-           Substack
-          </Link>
-          </li>
-        </ul>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.logo}>FullSpeed</h1>
+        </div>
+        <div className={styles.info}>
+          {/* <p>More info:</p> */}
+            <ul className={styles.ul}>
+              <li>
+              <Link href="https://twitter.com/nyashaspeed69">
+                Twitter
+              </Link>
+              </li>
+              <li>
+              <Link href="https://www.instagram.com/nyashaspeed69/">
+                Instagram
+              </Link>
+              </li>
+              {/* // link for substack */}
+              <li>
+              <Link href="https://substack.com/profile/12161427-nyashaspeed69">
+                Substack
+              </Link>
+              </li>
+            </ul>
+        </div>
+      </div>
+        <footer className={styles.footer}>
+            <p>© 2023. All rights reserved.</p>
+          </footer>
     </div>
   )
 }
